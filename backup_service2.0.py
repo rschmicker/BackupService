18893fdad993dd891d57d5ffc5c7016525c7a790
rschmicker <rhschmicker@gmail.com>
2015-11-02 12:50:01 -0500

    Added recursion for subdirectories

---
+++ backup_service2.0.py
@@ -0,0 +1,32 @@
+import sys
+import time
+import os
+from dirsync import sync
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+#place the directory of your google drive and one drive here
+google_drive="/Users/robertschmicker/Google Drive/College/Sophmore Year/First Semester"
+one_drive="/Users/robertschmicker/OneDrive/College/Sophmore Year/First Semester"
+
+class MyHandler(PatternMatchingEventHandler):
+    def process(self, event):
+        sync(args[0], google_drive, 'sync', purge=True, create=True)
+        sync(args[0], one_drive, 'sync', purge=True, create=True)
+        
+    def on_any_event(self, event):
+        self.process(event)
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    observer = Observer()
+    observer.schedule(MyHandler(), path=args[0] if args else '.', recursive=True)
+    observer.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    
+    observer.join()
